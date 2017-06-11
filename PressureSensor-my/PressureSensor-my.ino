711427a6714b05408b6aaf9efe523e2cad5689dc
doctor64 <doctor64@gmail.com>
2017-06-11 20:41:55 +0300

    Initial add existing code

---
+++ PressureSensor-my/PressureSensor-my.ino
@@ -0,0 +1,360 @@
+/**
+ * The MySensors Arduino library handles the wireless radio link and protocol
+ * between your home built sensors/actuators and HA controller of choice.
+ * The sensors forms a self healing radio network with optional repeaters. Each
+ * repeater and gateway builds a routing tables in EEPROM which keeps track of the
+ * network topology allowing messages to be routed to nodes.
+ *
+ * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+ * Copyright (C) 2013-2015 Sensnology AB
+ * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+ *
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ *******************************
+ *
+ * REVISION HISTORY
+ * Version 1.0 - Henrik Ekblad
+ * 
+ * DESCRIPTION
+ * Pressure sensor example using BMP085 module  
+ * http://www.mysensors.org/build/pressure
+ *
+ */
+
+// Enable debug prints to serial monitor
+#define MY_DEBUG 
+#define DEBUG_PRINT
+
+
+// Enable and select radio type attached
+#define MY_RADIO_NRF24
+#define MY_RF24_CHANNEL  113
+//#define MY_RADIO_RFM69
+//#define MY_NODE_ID 2
+
+#define MY_REPEATER_FEATURE
+
+#include <SPI.h>
+#include <MySensors.h>  
+#include <Wire.h>
+//#include <Adafruit_BMP085.h>
+#include <Sodaq_BMP085.h>
+#include <Sodaq_SHT2x.h>
+
+#define BARO_BMP_CHILD 0
+#define TEMP0_BMP_CHILD 1
+#define TEMP1_SHT_CHILD 2
+#define TEMP2_SHT_CHILD 3
+#define HUM0_SHT_CHILD 4
+
+const float ALTITUDE = 587; // <-- adapt this value to your own location's altitude.
+
+// Sleep time between reads (in seconds). Do not change this value as the forecast algorithm needs a sample every minute.
+const unsigned long SLEEP_TIME = 60000; 
+
+const char *weather[] = { "stable", "sunny", "cloudy", "unstable", "thunderstorm", "unknown" };
+enum FORECAST
+{
+	STABLE = 0,			// "Stable Weather Pattern"
+	SUNNY = 1,			// "Slowly rising Good Weather", "Clear/Sunny "
+	CLOUDY = 2,			// "Slowly falling L-Pressure ", "Cloudy/Rain "
+	UNSTABLE = 3,		// "Quickly rising H-Press",     "Not Stable"
+	THUNDERSTORM = 4,	// "Quickly falling L-Press",    "Thunderstorm"
+	UNKNOWN = 5			// "Unknown (More Time needed)
+};
+
+//Adafruit_BMP085 bmp = Adafruit_BMP085();      // Digital Pressure Sensor 
+Sodaq_BMP085 bmp;// = Adafruit_BMP085();      // Digital Pressure Sensor 
+
+float lastPressure = -1;
+float lastTemp_bmp = -1;
+float lastTemp_sht = -1;
+float lastHum = -1;
+float lastDew = -1;
+int lastForecast = -1;
+
+const int LAST_SAMPLES_COUNT = 5;
+float lastPressureSamples[LAST_SAMPLES_COUNT];
+
+// this CONVERSION_FACTOR is used to convert from Pa to kPa in forecast algorithm
+// get kPa/h be dividing hPa by 10 
+#define CONVERSION_FACTOR (1.0/10.0)
+
+int minuteCount = 0;
+bool firstRound = true;
+// average value is used in forecast algorithm.
+float pressureAvg;
+// average after 2 hours is used as reference value for the next iteration.
+float pressureAvg2;
+
+float dP_dt;
+bool metric;
+MyMessage tempBMPMsg(TEMP0_BMP_CHILD, V_TEMP);
+MyMessage pressureBMPMsg(BARO_BMP_CHILD, V_PRESSURE);
+MyMessage forecastBMPMsg(BARO_BMP_CHILD, V_FORECAST);
+MyMessage tempSHTMsg(TEMP1_SHT_CHILD, V_TEMP);
+MyMessage humSHTMsg(HUM0_SHT_CHILD, V_HUM);
+MyMessage dewSHTMsg(TEMP2_SHT_CHILD, V_TEMP);
+
+void setup() 
+{
+  bmp.begin();
+	/*if (!bmp.begin()) 
+	{
+		Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+		while (1) {}
+	}*/
+	metric = getControllerConfig().isMetric;
+}
+
+void presentation()  {
+  // Send the sketch version information to the gateway and Controller
+  sendSketchInfo("Pressure/temp/hum Sensor", "1.2");
+
+  // Register sensors to gw (they will be created as child devices)
+  present(BARO_BMP_CHILD, S_BARO, "Barometer");
+  present(TEMP0_BMP_CHILD, S_TEMP, "Temperature bmp");
+  present(TEMP1_SHT_CHILD, S_TEMP, "Temperature sht" );
+  present(HUM0_SHT_CHILD, S_HUM, "Humidity");
+  present(TEMP2_SHT_CHILD, S_TEMP, "Dew point");
+}
+
+void loop() 
+{
+	//float pressure = bmp.readPressure(ALTITUDE) / 100.0;
+  float pressure = bmp.readPressure() / 100.0;
+	float temperature_bmp = bmp.readTemperature();
+  float temperature_sht = SHT2x.GetTemperature();
+  float dew_sht = SHT2x.GetDewPoint();
+  float hum_sht = SHT2x.GetHumidity();
+  
+	if (!metric) 
+	{
+		// Convert to fahrenheit
+		temperature_bmp = temperature_bmp * 9.0 / 5.0 + 32.0;
+    temperature_sht = temperature_sht * 9.0 / 5.0 + 32.0;
+    dew_sht = dew_sht * 9.0 / 5.0 + 32.0;
+	}
+
+	int forecast = sample(pressure);
+  
+#ifdef DEBUG_PRINT
+	Serial.print("Temperature BMP = ");
+	Serial.print(temperature_bmp);
+	Serial.println(metric ? " *C" : " *F");
+	Serial.print("Pressure = ");
+	Serial.print(pressure);
+	Serial.println(" hPa");
+	Serial.print("Forecast = ");
+	Serial.println(weather[forecast]);
+  Serial.print("Temperature SHT = ");
+  Serial.print(temperature_sht);
+  Serial.println(metric ? " *C" : " *F");
+  Serial.print("Dew point = ");
+  Serial.print(dew_sht);
+  Serial.println(metric ? " *C" : " *F");
+  Serial.print("Humidity SHT = ");
+  Serial.print(hum_sht);
+  Serial.println(" %");
+#endif
+
+	if (temperature_bmp != lastTemp_bmp) 
+	{
+		send(tempBMPMsg.set(temperature_bmp, 1));
+		lastTemp_bmp = temperature_bmp;
+	}
+  
+  if (temperature_sht != lastTemp_sht) 
+  {
+    send(tempSHTMsg.set(temperature_sht, 1));
+    lastTemp_sht = temperature_sht;
+  }
+  
+  if (dew_sht != lastDew) 
+  {
+    send(dewSHTMsg.set(dew_sht, 1));
+    lastDew = dew_sht;
+  }
+
+  if (hum_sht != lastHum) 
+  {
+    send(humSHTMsg.set(hum_sht, 1));
+    lastHum = hum_sht;
+  }  
+
+	if (pressure != lastPressure) 
+	{
+		send(pressureBMPMsg.set(pressure, 0));
+		lastPressure = pressure;
+	}
+
+	if (forecast != lastForecast)
+	{
+		send(forecastBMPMsg.set(weather[forecast]));
+		lastForecast = forecast;
+	}
+
+	smartSleep(SLEEP_TIME);
+}
+
+float getLastPressureSamplesAverage()
+{
+	float lastPressureSamplesAverage = 0;
+	for (int i = 0; i < LAST_SAMPLES_COUNT; i++)
+	{
+		lastPressureSamplesAverage += lastPressureSamples[i];
+	}
+	lastPressureSamplesAverage /= LAST_SAMPLES_COUNT;
+
+	return lastPressureSamplesAverage;
+}
+
+
+
+// Algorithm found here
+// http://www.freescale.com/files/sensors/doc/app_note/AN3914.pdf
+// Pressure in hPa -->  forecast done by calculating kPa/h
+int sample(float pressure)
+{
+	// Calculate the average of the last n minutes.
+	int index = minuteCount % LAST_SAMPLES_COUNT;
+	lastPressureSamples[index] = pressure;
+
+	minuteCount++;
+	if (minuteCount > 185)
+	{
+		minuteCount = 6;
+	}
+
+	if (minuteCount == 5)
+	{
+		pressureAvg = getLastPressureSamplesAverage();
+	}
+	else if (minuteCount == 35)
+	{
+		float lastPressureAvg = getLastPressureSamplesAverage();
+		float change = (lastPressureAvg - pressureAvg) * CONVERSION_FACTOR;
+		if (firstRound) // first time initial 3 hour
+		{
+			dP_dt = change * 2; // note this is for t = 0.5hour
+		}
+		else
+		{
+			dP_dt = change / 1.5; // divide by 1.5 as this is the difference in time from 0 value.
+		}
+	}
+	else if (minuteCount == 65)
+	{
+		float lastPressureAvg = getLastPressureSamplesAverage();
+		float change = (lastPressureAvg - pressureAvg) * CONVERSION_FACTOR;
+		if (firstRound) //first time initial 3 hour
+		{
+			dP_dt = change; //note this is for t = 1 hour
+		}
+		else
+		{
+			dP_dt = change / 2; //divide by 2 as this is the difference in time from 0 value
+		}
+	}
+	else if (minuteCount == 95)
+	{
+		float lastPressureAvg = getLastPressureSamplesAverage();
+		float change = (lastPressureAvg - pressureAvg) * CONVERSION_FACTOR;
+		if (firstRound) // first time initial 3 hour
+		{
+			dP_dt = change / 1.5; // note this is for t = 1.5 hour
+		}
+		else
+		{
+			dP_dt = change / 2.5; // divide by 2.5 as this is the difference in time from 0 value
+		}
+	}
+	else if (minuteCount == 125)
+	{
+		float lastPressureAvg = getLastPressureSamplesAverage();
+		pressureAvg2 = lastPressureAvg; // store for later use.
+		float change = (lastPressureAvg - pressureAvg) * CONVERSION_FACTOR;
+		if (firstRound) // first time initial 3 hour
+		{
+			dP_dt = change / 2; // note this is for t = 2 hour
+		}
+		else
+		{
+			dP_dt = change / 3; // divide by 3 as this is the difference in time from 0 value
+		}
+	}
+	else if (minuteCount == 155)
+	{
+		float lastPressureAvg = getLastPressureSamplesAverage();
+		float change = (lastPressureAvg - pressureAvg) * CONVERSION_FACTOR;
+		if (firstRound) // first time initial 3 hour
+		{
+			dP_dt = change / 2.5; // note this is for t = 2.5 hour
+		}
+		else
+		{
+			dP_dt = change / 3.5; // divide by 3.5 as this is the difference in time from 0 value
+		}
+	}
+	else if (minuteCount == 185)
+	{
+		float lastPressureAvg = getLastPressureSamplesAverage();
+		float change = (lastPressureAvg - pressureAvg) * CONVERSION_FACTOR;
+		if (firstRound) // first time initial 3 hour
+		{
+			dP_dt = change / 3; // note this is for t = 3 hour
+		}
+		else
+		{
+			dP_dt = change / 4; // divide by 4 as this is the difference in time from 0 value
+		}
+		pressureAvg = pressureAvg2; // Equating the pressure at 0 to the pressure at 2 hour after 3 hours have past.
+		firstRound = false; // flag to let you know that this is on the past 3 hour mark. Initialized to 0 outside main loop.
+	}
+
+	int forecast = UNKNOWN;
+	if (minuteCount < 35 && firstRound) //if time is less than 35 min on the first 3 hour interval.
+	{
+		forecast = UNKNOWN;
+	}
+	else if (dP_dt < (-0.25))
+	{
+		forecast = THUNDERSTORM;
+	}
+	else if (dP_dt > 0.25)
+	{
+		forecast = UNSTABLE;
+	}
+	else if ((dP_dt > (-0.25)) && (dP_dt < (-0.05)))
+	{
+		forecast = CLOUDY;
+	}
+	else if ((dP_dt > 0.05) && (dP_dt < 0.25))
+	{
+		forecast = SUNNY;
+	}
+	else if ((dP_dt >(-0.05)) && (dP_dt < 0.05))
+	{
+		forecast = STABLE;
+	}
+	else
+	{
+		forecast = UNKNOWN;
+	}
+
+	// uncomment when debugging
+	//Serial.print(F("Forecast at minute "));
+	//Serial.print(minuteCount);
+	//Serial.print(F(" dP/dt = "));
+	//Serial.print(dP_dt);
+	//Serial.print(F("kPa/h --> "));
+	//Serial.println(weather[forecast]);
+
+	return forecast;
+}
---
+++ devDuino4basic/devDuino4basic.ino
@@ -0,0 +1,372 @@
+/**
+ * The MySensors Arduino library handles the wireless radio link and protocol
+ * between your home built sensors/actuators and HA controller of choice.
+ * The sensors forms a self healing radio network with optional repeaters. Each
+ * repeater and gateway builds a routing tables in EEPROM which keeps track of the
+ * network topology allowing messages to be routed to nodes.
+ *
+ * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+ * Copyright (C) 2013-2015 Sensnology AB
+ * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+ *
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ *******************************
+ *
+ * REVISION HISTORY
+ * Version 1.0 - Henrik Ekblad
+ * 
+ * DESCRIPTION
+ * Pressure sensor example using BMP085 module  
+ * http://www.mysensors.org/build/pressure
+ *
+ */
+
+// Enable debug prints to serial monitor
+#define MY_DEBUG 
+#define DEBUG_PRINT
+
+// Enable and select radio type attached
+#define MY_RADIO_NRF24
+//#define MY_RADIO_RFM69
+#define MY_RF24_CE_PIN 8
+#define MY_RF24_CS_PIN 7
+//#define MY_RF24_PA_LEVEL RF24_PA_LOW
+#define MY_RF24_CHANNEL  113
+//#define MY_NODE_ID 2
+
+// Pin definitions
+#define DUINO4_LED 9
+#define DUINO4_BUTTON 4
+
+// How many milli seconds between each measurement
+#define MEASURE_INTERVAL 50000//50000 //for Real Work 50 sec for internal use Home
+
+// FORCE_TRANSMIT_INTERVAL, this number of times of wakeup, the sensor is forced to report all values to the controller
+#define FORCE_TRANSMIT_INTERVAL 30 
+
+#define SKETCH_NAME "devDuino4 basic Sensor"
+#define SKETCH_VERSION "1.4"
+
+#include <SPI.h>
+#include <MySensors.h>  
+#include <Wire.h>
+#include "SparkFunHTU21D.h"
+#include <Bounce2.h>
+#include <RunningAverage.h>
+
+#define TEMP0_HTU_CHILD 0
+#define HUM0_HTU_CHILD 1
+#define BATT_CHILD 2
+#define LED_CHILD 3
+#define BUTTON_CHILD 4
+
+HTU21D htu;
+Bounce debouncer = Bounce(); 
+
+//float lastTemp_htu = -1;
+//float lastHum = -1;
+
+bool metric;
+// LED blinks during data transmission. Greater battery energy consumption!
+//#define LED_BLINK_WAIT_TRANSMIT  //turn off for external use (Street)
+
+#define TEMP_TRANSMIT_THRESHOLD 0.5
+#define HUMI_TRANSMIT_THRESHOLD 2//0.5
+
+MyMessage tempHTUMsg(TEMP0_HTU_CHILD, V_TEMP);
+MyMessage humHTUMsg(HUM0_HTU_CHILD, V_HUM);
+//MyMessage buttonMsg(BUTTON_CHILD, V_STATUS);
+MyMessage ledMsg(LED_CHILD, V_STATUS);
+#ifdef BATT_CHILD
+MyMessage msgBatt(BATT_CHILD, V_VOLTAGE);
+#endif
+
+//#define LED_ON 1  // GPIO value to write to turn on attached relay
+//#define LED_OFF 0 // GPIO value to write to turn off attached relay
+
+// Global settings
+int measureCount = 0;
+int measureCountTemp = 0;
+int measureCountHum = 0;
+
+//boolean ota_enabled = false; 
+int sendBattery = 0;
+boolean highfreq = true;
+boolean transmission_occured = false;
+
+// Storage of old measurements
+float lastTemperature = 0;
+int lastHumidity = 0;
+long lastBattery = 0;
+float sendVCC; 
+#define AVERAGES 5
+RunningAverage raHum(AVERAGES);
+
+void setup() 
+{
+  Serial.begin(MY_BAUD_RATE);
+  htu.begin();
+  // initialize digital pin 9 as an output.
+  pinMode(DUINO4_LED, OUTPUT);
+  // Setup the button
+  pinMode(DUINO4_BUTTON,INPUT);
+  // Activate internal pull-up
+  digitalWrite(DUINO4_BUTTON,HIGH);
+  
+  // After setting up the button, setup debouncer
+  debouncer.attach(DUINO4_BUTTON);
+  debouncer.interval(5);
+
+  debouncer.update();
+  // Get the update value
+  int value = debouncer.read();
+  if (value==0) {
+    Serial.println("Started clearing. Please wait...");
+    for (int i=0; i<EEPROM_LOCAL_CONFIG_ADDRESS; i++) {
+      hwWriteConfig(i,0xFF);
+      if (i%16==0) {   
+        digitalWrite(DUINO4_LED, HIGH);
+        wait(50);
+        digitalWrite(DUINO4_LED, LOW);
+        wait(50);
+      }
+    }
+    Serial.println("Clearing done. You're ready to go!");
+    void(* resetFunc) (void) = 0;//declare reset function at address 0
+    resetFunc(); //call reset 
+  }
+    
+  metric = getControllerConfig().isMetric;
+  raHum.clear();
+  sendTempHumidityMeasurements(true);
+  sendBattLevel(true);
+  
+  digitalWrite(DUINO4_LED, HIGH);
+  wait(500);
+  send(msgBatt.set(sendVCC,2));
+  digitalWrite(DUINO4_LED, LOW);
+	
+}
+
+void presentation()  {
+  // Send the sketch version information to the gateway and Controller
+  sendSketchInfo(SKETCH_NAME, SKETCH_VERSION);   
+
+  // Register sensors to gw (they will be created as child devices)
+  present(TEMP0_HTU_CHILD, S_TEMP, "Temperature");
+  present(HUM0_HTU_CHILD, S_HUM, "Humidity");
+  //present(BUTTON_CHILD, S_BINARY, "Button");
+  present(LED_CHILD, S_BINARY,"LED");
+#ifdef BATT_CHILD
+   present(BATT_CHILD, S_MULTIMETER, "VOLT");
+#endif    
+}  
+
+void loop() 
+{
+  measureCount ++;
+  measureCountTemp ++;
+  measureCountHum ++;
+
+  sendBattery ++;
+  
+  bool forceTransmit = false;
+  transmission_occured = false;
+  #ifndef DEBUG_PRINT
+  if ((measureCount == 3) && highfreq) 
+  {
+    clock_prescale_set(clock_div_8); // Switch to 1Mhz for the reminder of the sketch, save power.
+    highfreq = false;
+  } 
+  #endif
+  
+  if (measureCount > FORCE_TRANSMIT_INTERVAL) { // force a transmission
+    forceTransmit = true; 
+    measureCount = 0;
+  }
+    
+  sendTempHumidityMeasurements(forceTransmit);
+  
+  if (sendBattery > 40) 
+  {
+     sendBattLevel(forceTransmit); // Not needed to send battery info that often
+     sendBattery = 0;
+  }
+
+	//smartSleep(MEASURE_INTERVAL);
+  sleep(MEASURE_INTERVAL);
+	//sleep(/*digitalPinToInterrupt(2)*/0, RISING , MEASURE_INTERVAL);
+  //wait(MEASURE_INTERVAL);
+}
+void receive(const MyMessage &message)
+{
+  // We only expect one type of message from controller. But we better check anyway.
+  // And acks are not accepted as control messages
+  if (message.type==V_STATUS && message.sensor==LED_CHILD && !mGetAck(message)) {
+    // Change relay state
+    digitalWrite(DUINO4_LED, message.getBool()?HIGH:LOW);
+    // Store state in eeprom
+    //saveState(message.sensor, message.getBool());
+    // Write some debug info
+    #ifdef DEBUG_PRINT
+    Serial.print("Incoming change for led:");
+    Serial.print(message.sensor);
+    Serial.print(", New status: ");
+    Serial.println(message.getBool());
+    #endif
+  }
+}
+
+/*********************************************
+ * Sends temperature and humidity sensor
+ *
+ * Parameters
+ * - force : Forces transmission of a value (even if it's the same as previous measurement)
+ *********************************************/
+void sendTempHumidityMeasurements(bool force)
+{
+  bool tx = force;
+  bool tx1 = force;
+
+  //get the Temperature and Humidity from the onboard sensor.
+  float temp = htu.readTemperature();
+  int humidity = htu.readHumidity();
+  if (!metric) 
+  {
+    // Convert to fahrenheit
+    temp = temp * 9.0 / 5.0 + 32.0;
+  }
+  
+  raHum.addValue(humidity);
+  
+  float diffTemp = abs(lastTemperature - temp);
+  float diffHum = abs(lastHumidity - raHum.getAverage());
+
+  #ifdef DEBUG_PRINT
+  Serial.print(F("TempDiff :"));Serial.println(diffTemp);
+  Serial.print(F("HumDiff  :"));Serial.println(diffHum); 
+  #endif
+
+  if (diffTemp > TEMP_TRANSMIT_THRESHOLD) tx = true;
+  if (tx) {
+    measureCountTemp = 0;  
+    // LED 
+  #ifdef LED_BLINK_WAIT_TRANSMIT
+    digitalWrite(DUINO4_LED, HIGH);      
+    send(tempHTUMsg.set(temp,1));
+    digitalWrite(DUINO4_LED, LOW);
+  }
+  #else
+    send(tempHTUMsg.set(temp,1));
+  }
+  #endif  
+  
+  if (isnan(diffHum)) tx1 = true; 
+  if (diffHum > HUMI_TRANSMIT_THRESHOLD) tx1 = true;
+
+  if (tx1) {
+     measureCountHum = 0;
+     // LED 
+     #ifdef LED_BLINK_WAIT_TRANSMIT
+     digitalWrite(DUINO4_LED, HIGH);      
+     send(humHTUMsg.set(humidity));
+     digitalWrite(DUINO4_LED, LOW);
+     #else
+     send(humHTUMsg.set(humidity));
+     #endif 
+  }  
+  #ifdef DEBUG_PRINT
+  Serial.print("Temperature HTU = ");
+  Serial.print(temp);
+  Serial.println(metric ? " *C" : " *F");
+  Serial.print("Humidity HTU = ");
+  Serial.print(humidity);
+  Serial.println(" %");
+  Serial.print("measureCount: ");Serial.println(measureCount);
+  Serial.print("sendBattery: ");Serial.println(sendBattery);
+  #endif
+
+
+  lastTemperature = temp;
+  lastHumidity = humidity;
+  transmission_occured = true;
+}
+/********************************************
+ *
+ * Sends battery information (battery percentage)
+ *
+ * Parameters
+ * - force : Forces transmission of a value
+ *
+ *******************************************/
+void sendBattLevel(bool force)
+{ 
+  if (force) lastBattery = -1;
+  long vcc = readVcc();
+  sendVCC = vcc/1000.0;
+
+  if (vcc != lastBattery) {
+    lastBattery = vcc;
+
+  #ifdef BATT_CHILD
+    send(msgBatt.set(sendVCC,2));
+  #endif
+
+    // Calculate percentage
+    vcc = vcc - 1900; // subtract 1.9V from vcc, as this is the lowest voltage we will operate at
+    
+    long percent = vcc / 14.0;
+    
+    #ifdef DEBUG_PRINT
+    Serial.print("Batt voltage = ");
+    Serial.print(sendVCC);
+    Serial.println(" V");
+    Serial.print("Batt percentage = ");
+    Serial.print(percent);
+    Serial.println(" %");
+    #endif
+    
+    sendBatteryLevel(percent);
+    transmission_occured = true;
+  }
+}
+/*******************************************
+ *
+ * Internal battery ADC measuring 
+ *
+ *******************************************/
+long readVcc() {
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+    ADMUX = _BV(MUX5) | _BV(MUX0);
+  #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+    ADcdMUX = _BV(MUX3) | _BV(MUX2);
+  #else
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #endif  
+ 
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA,ADSC)); // measuring
+ 
+  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
+  uint8_t high = ADCH; // unlocks both
+ 
+  long result = (high<<8) | low;
+ 
+//  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+   result = 1149900L / result; // Calculate Vcc (in mV); 1 149 900 = 1.124*1023*1000
+  return result; // Vcc in millivolts
+ 
+}
+
+
